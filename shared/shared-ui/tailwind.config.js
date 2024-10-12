047038e3c12233419cd833aa5e1f3a658c299f69
Papon <papon.gun2@gmail.com>
2024-10-12 19:31:15 +0700

    Shared tailwinds

---
+++ shared/shared-ui/tailwind.config.js
@@ -0,0 +1,31 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#f0f4ff',
+          200: '#d9e4ff',
+          300: '#a6c1ff',
+          400: '#598bff',
+          500: '#3366ff',
+          600: '#274bdb',
+          700: '#1a34b8',
+          800: '#102694',
+          900: '#091c7a',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
