68e72fbf082ef51dc5137255e14986f42ec84abc
Papon <papon.gun2@gmail.com>
2024-10-15 22:36:43 +0700

    [DEL] Trash

--- shared/shared-ui/src/lib/shared-ui.tsx
+++ shared/shared-ui/src/lib/shared-ui.tsx
@@ -1,15 +1,3 @@
-import styled from 'tailwind';
-
-const StyledSharedUi = styled.div`
-  color: pink;
-`;
-
-export function SharedUi() {
-  return (
-    <StyledSharedUi>
-      <h1>Welcome to SharedUi!</h1>
-    </StyledSharedUi>
-  );
+export default function SharedUI() {
+  return <div></div>;
 }
-
-export default SharedUi;
