bf77d7f09af5a1ef9b022d867eae496ad65d998b
Papon <papon.gun2@gmail.com>
2024-10-21 23:07:55 +0700

    [ADD] Send request reservation to stomp

--- apps/web/public/src/app/data/repositories/request_reservation/postRequestReservationRepository.ts
+++ apps/web/public/src/app/data/repositories/request_reservation/postRequestReservationRepository.ts
@@ -3,18 +3,23 @@ import {
   RequestReservationForm,
   RequestReservationResponse,
 } from '@ce-lab-mgmt/api-interfaces';
-import { api } from '../../adapter/axios';
 import { AxiosError } from 'axios';
+import { sendToQueue } from '../../adapter/stomp';
 
+// CHANGE TO REST API ON PRODUCTION
 export default async function postRequestReservationRepository(
   data: RequestReservationForm
 ): Promise<BaseResponse<RequestReservationResponse>> {
   try {
-    const result = await api.post<BaseResponse<RequestReservationResponse>>(
-      '/reservation',
-      data
-    );
-    return result.data;
+    const rabbitResult = await sendToQueue('reservation', data);
+
+    if (rabbitResult.code !== 200) {
+      return new BaseResponse({
+        error: { code: rabbitResult.code },
+      });
+    }
+
+    return new BaseResponse({ data: { code: 200 } });
   } catch (error) {
     if (error instanceof AxiosError && error.response) {
       return new BaseResponse({ error: error.response.data.error });
