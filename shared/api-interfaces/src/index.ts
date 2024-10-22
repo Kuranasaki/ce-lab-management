34968698160cc75a51fb62c42f5987d2f278bc85
Kuranasaki@M3Max <ironpan10@hotmail.com>
2024-10-22 19:53:33 +0700

    temp resv api

--- shared/api-interfaces/src/index.ts
+++ shared/api-interfaces/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/api-interfaces';
-export * as ReservationDTO from './lib/reservation';
+export * from './lib/reservation';
 export * from './lib/pricing';
-export * from './lib/request_reservation';
