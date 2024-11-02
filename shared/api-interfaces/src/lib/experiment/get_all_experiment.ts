5dd41cc44b6bde14caa3d0b7d98442feca845ed5
Papon <papon.gun2@gmail.com>
2024-11-02 15:33:58 +0700

    [ADD] QueryParam all experiment by reservationID, and return professor with it

--- shared/api-interfaces/src/lib/experiment/get_all_experiment.ts
+++ shared/api-interfaces/src/lib/experiment/get_all_experiment.ts
@@ -5,16 +5,23 @@ import { t } from 'elysia';
 Request
 - Get: /experiment
 - No request body
+- QueryParams: GetAllExperimentRequestParamsSchema
 
 Response
 - Status: 200
 - Body: GetAllExperimentResponseSchema
 */
 
+export const GetAllExperimentRequestParamsSchema = t.Object({
+  reservationID: t.Optional(t.String()),
+});
+
 export const GetAllExperimentResponseSchema = t.Array(
   t.Object({
     id: t.String(),
     reservationID: t.String(),
+    assignedProfessorID: t.String(),
+    assignedProfessorName: t.String(),
     testItemID: t.String(),
     isDone: t.Boolean(),
   })
