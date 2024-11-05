72ab99acbc0cc57cfa61a6bbe8d1f1b9eb860b96
papon <papon.kle@gmail.com>
2024-11-05 11:22:47 +0700

    [ADD] get one prof/staff

---
+++ shared/api-interfaces/src/lib/users/get_one_professor.ts
@@ -0,0 +1,17 @@
+import { t } from 'elysia';
+
+// Get one professor
+/*
+Request
+- GET: /users/professor/:id
+- No request body
+
+Response
+- Status: 200
+- Body: GetOneProfessorResponseSchema
+*/
+export const GetOneProfessorResponseSchema = t.Object({
+  id: t.String(),
+  name: t.String(),
+  email: t.String(),
+});
