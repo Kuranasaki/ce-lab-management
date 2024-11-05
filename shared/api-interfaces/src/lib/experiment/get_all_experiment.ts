6ce99147db3ba61686dc1349af9876a2f60263d0
papon <papon.kle@gmail.com>
2024-11-05 09:05:29 +0700

    [ADD] add field to experiment

--- shared/api-interfaces/src/lib/experiment/get_all_experiment.ts
+++ shared/api-interfaces/src/lib/experiment/get_all_experiment.ts
@@ -22,11 +22,14 @@ export const GetAllExperimentResponseSchema = t.Array(
     reservationID: t.String(), // Reservation ID
     testItemID: t.String(), // Pointer to testItem in reservation
     testName: t.String(), // Test Name e.g. Tensile test of prestressed wire and presstressed strand: wire ø 7 mm
+    testAmount: t.Number(), // Test Amount
     testDetails: t.Nullable(t.String()), // Test Details Added by customer
     testNote: t.Nullable(t.String()), // Test Note Added by customer
     assignedProfessorID: t.Nullable(t.String()),
     assignedProfessorName: t.Nullable(t.String()),
+    assignedAt: t.Date(),
     markedAsDone: t.Boolean(),
+    markedAsDoneAt: t.Nullable(t.Date()),
     certificateUploadedAt: t.Nullable(t.Date()),
   })
 );
