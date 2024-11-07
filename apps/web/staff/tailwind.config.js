83d5c42ddc5a7b8157aeeb873d65c609fd40c5ba
papon <papon.kle@gmail.com>
2024-11-07 14:25:17 +0700

    [ADD] Config Tailwind

--- apps/web/staff/tailwind.config.js
+++ apps/web/staff/tailwind.config.js
@@ -1,8 +1,10 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
+const base = require('../../../shared/shared-ui/tailwind.config');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [base],
   content: [
     join(
       __dirname,
@@ -10,8 +12,6 @@ module.exports = {
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [],
 };
