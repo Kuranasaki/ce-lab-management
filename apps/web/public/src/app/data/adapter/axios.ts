7ae9b792f6ba392464780304d2410f1829b24563
Punyaphat Surakiatkamjorn <punyaphat.sura@gmail.com>
2024-11-13 06:21:38 +0700

    wip

--- apps/web/public/src/app/data/adapter/axios.ts
+++ apps/web/public/src/app/data/adapter/axios.ts
@@ -4,6 +4,10 @@ import { App as ReservationServiceType } from '@ce-lab-mgmt/services/reservation
 import { treaty } from '@elysiajs/eden';
 import { AuthClass } from '../../hooks/tokenClass';
 
+const userId = AuthClass.getUserId();
+
+const endpoint = process.env.VITE_API_URL || 'http://localhost:3000';
+
 // To be config later
 export const api = axios.create({
   baseURL: 'http://localhost:3000',
@@ -12,14 +16,15 @@ export const api = axios.create({
   },
 });
 
-export const pricingApi = treaty<PricingServiceType>('http://localhost:3001', {
+export const pricingApi = treaty<PricingServiceType>(endpoint, {
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${AuthClass.getToken()}`,
-    'x-user-id': 'bL2qciSODSbddrhgCjQvCRnVC4i2',
+    'x-user-id': userId,
     'x-user-roles': 'customer',
   },
 });
+
 export const reservationApi = treaty<ReservationServiceType>(
   'http://localhost:3000',
   {
