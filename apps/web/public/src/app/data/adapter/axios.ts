8a09f9078dd4247a39ac0268dee39e8c20aa9d9e
Punyaphat Surakiatkamjorn <punyaphat.sura@gmail.com>
2024-11-13 03:25:48 +0700

    mai ru wa gu tum a rai u TT

--- apps/web/public/src/app/data/adapter/axios.ts
+++ apps/web/public/src/app/data/adapter/axios.ts
@@ -2,16 +2,33 @@ import axios from 'axios';
 import { PricingServiceType } from '@ce-lab-mgmt/services/pricing';
 import { App as ReservationServiceType } from '@ce-lab-mgmt/services/reservation';
 import { treaty } from '@elysiajs/eden';
+import { AuthClass } from '../../hooks/tokenClass';
+
+const userId = AuthClass.getUserId();
+
+const endpoint = process.env.API_ENDPOINT || 'http://localhost:3000';
 
 // To be config later
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: endpoint,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export const pricingApi = treaty<PricingServiceType>('http://localhost:3001');
-export const reservationApi = treaty<ReservationServiceType>(
-  'http://localhost:3000'
-);
+export const pricingApi = treaty<PricingServiceType>(endpoint, {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${AuthClass.getToken()}`,
+    'x-user-id': userId,
+    'x-user-roles': 'customer',
+  },
+});
+export const reservationApi = treaty<ReservationServiceType>(endpoint, {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${AuthClass.getToken()}`,
+    'x-user-id': 'bL2qciSODSbddrhgCjQvCRnVC4i2',
+    'x-user-roles': 'customer',
+  },
+});
